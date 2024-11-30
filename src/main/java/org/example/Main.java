3e7c96331af6d0d678580a41f0a12615a898c17d
unknown <anna.elisabeth@gmail.com>
2024-11-30 17:54:18 +0000

    methods in main added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,149 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
 
+if(args.length == 0){
+    performCalculations();
+} else if (args.length == 1 && args[0].equals("interactive"))
+    executeInteractively();
+else if(args.length == 3)
+    handleCommandLine(args);
+else
+    System.out.println("Operation code and two numeric values are required");
+}
 
+static void performCalculations(){
+        MathEquation[] equations = new MathEquation[4];
+           equations[0] = new MathEquation (MathOperation.DIVIDE, 100.0, 50.0);
+    equations[1] = new MathEquation (MathOperation.ADD, 25.0, 92.0);
+    equations[2] = new MathEquation (MathOperation.SUBTRACT, 255.0, 92.0);
+    equations[3] = new MathEquation (MathOperation.MULTIPLY, 2.0, 2.0);
 
+    for(MathEquation equation : equations){
+        equation.execute();
+        System.out.println(equation);
+    }
+    System.out.println("Average result= " + MathEquation.getAverageResult());
+        useOverloads();
+    }
+
+    static void useOverloads(){
+        System.out.println();
+        System.out.println("Using execute overloads");
+        System.out.println();
+
+        MathEquation equationOverload = new MathEquation(MathOperation.DIVIDE);
+        double leftDouble = 9.0d;
+        double rightDouble = 4.0d;
+        equationOverload.execute(leftDouble, rightDouble);
+        System.out.println("Overload results with doubles: " + equationOverload.getResult());
+
+        int leftInt = 9;
+        int rightInt = 4;
+        equationOverload.execute(leftInt, rightInt);
+        System.out.println("Overload results with ints: " + equationOverload.getResult());
+
+    }
+
+    static void executeInteractively(){
+        System.out.println("Please enter an operation and two numbers");
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        String[] parts = userInput.split(" ");
+        performOperation(parts);
+    }
+
+    private static void performOperation(String[] parts){
+        MathOperation opCode = MathOperation.valueOf(parts[0].toUpperCase());
+        double leftVal =  valueFromWord(parts[1]);
+        double rightVal = valueFromWord(parts[2]);
+        MathEquation equation = new MathEquation(opCode, leftVal, rightVal);
+        equation.execute();
+        System.out.println(equation);
+    }
+
+    private static void displayResult(char opCode, double leftVal, double rightVal, double result){
+        char symbol = symbolFromOpCode(opCode);
+        StringBuilder builder = new StringBuilder(20);
+        builder.append(leftVal);
+        builder.append(" ");
+        builder.append(symbol);
+        builder.append(" ");
+        builder.append(rightVal);
+        builder.append(" = ");
+        builder.append(result);
+        String output = builder.toString();
+        System.out.println(output);
+    }
+
+    private static char symbolFromOpCode(char opCode){
+        char[] opCodes = {'a', 's', 'm', 'd'};
+        char[] symbols = {'+', '-', '*', '/'};
+        char symbol = ' ';
+        for(int index =0; index < opCodes.length; index++){
+            if(opCode == opCodes[index]){
+                symbol = symbols[index];
+                break;
+            }
         }
+        return symbol;
+    }
+
+    private static void handleCommandLine(String[] args){
+        char opCode = args[0].charAt(0);
+        double leftVal = Double.parseDouble(args[1]);
+        double rightVal = Double.parseDouble(args[2]);
+        double result = execute(opCode, leftVal, rightVal);
+        System.out.println(result);
+    }
+
+    static double execute(char opCode, double leftVal, double rightVal){
+        double result;
+        switch (opCode){
+            case 'a':
+                result = leftVal + rightVal;
+                break;
+            case 's':
+                result = leftVal - rightVal;
+                break;
+            case 'm':
+                result = leftVal * rightVal;
+                break;
+            case 'd':
+                result = rightVal != 0 ? leftVal / rightVal : 0.0d;
+                break;
+            default:
+                System.out.println("Invalid opCode: " + opCode);
+                result = 0.0d;
+                break;
+        }
+        return result;
+    }
+    static char opCodeFromString(String operationName){
+        char opCode = operationName.charAt(0);
+        return opCode;
+    }
+
+    static double valueFromWord(String word){
+        String[] numberWords = {
+                "zero", "one", "two", "three", "four",
+                "five", "six", "seven", "eight", "nine"
+        };
+        boolean isValueSet = false;
+        double value = 0d;
+        for(int index =0; index< numberWords.length; index++){
+            if(word.equals(numberWords[index])){
+                value =index;
+                isValueSet = true;
+                break;
+            }
+        }
+        if(!isValueSet)
+            value = Double.parseDouble(word);
+        return value;
     }
 }
